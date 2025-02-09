17b16ca8b55bfc9aac8ee30bf9a3a94eb8233fe8
cahdmus <67350180+cahdmus@users.noreply.github.com>
2025-02-09 19:50:24 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+if (process.env.NODE_ENV !== 'production') {
+    console.log('-------------------------------------');
+    console.log('Sup boi, we are in DEVELOPMENT MODE !');
+    console.log('-------------------------------------');
+}
+
+function component() {
+    const element = document.createElement('pre');
+    element.innerHTML = 'What we doin today ?';
+
+    return element;
+}
+
+document.body.appendChild(component());
