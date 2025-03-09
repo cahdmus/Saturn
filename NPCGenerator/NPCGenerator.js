17909bc1806359eae461bfa27ba9e8dcb17087f1
cahdmus <bayingpan@gmail.com>
2025-03-09 14:14:35 +0100

    added default img when there is none

--- NPCGenerator/NPCGenerator.js
+++ NPCGenerator/NPCGenerator.js
@@ -276,7 +276,8 @@ class NPC {
         let socialFilteredList = ageFilteredList.filter((image) => image.tags.socialClass.includes(socialClass));   
 
         const avatar = roll.from(socialFilteredList);
-        return avatar.url
+
+        return (avatar.url === undefined) ? 'e0e34f8928bea383fa8a4351702b4b19.jpg': avatar.url;
     }
 }
 
